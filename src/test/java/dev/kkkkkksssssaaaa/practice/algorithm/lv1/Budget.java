73005eadc7689a02422321f6294c02c589a3774e
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-15 23:42:28 +0900

    프로그래머스-예산 sort 추가

--- src/test/java/dev/kkkkkksssssaaaa/practice/algorithm/lv1/Budget.java
+++ src/test/java/dev/kkkkkksssssaaaa/practice/algorithm/lv1/Budget.java
@@ -1,14 +1,18 @@
 package dev.kkkkkksssssaaaa.practice.algorithm.lv1;
 
+import java.util.Arrays;
+
 // 예산
 class Budget {
 
     public int solution(int[] d, int budget) {
         int count = 0;
 
-        for (int i : d) {
-            if (budget - i >= 0) {
-                budget -= i;
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            if (budget - d[i] >= 0) {
+                budget -= d[i];
                 count++;
             }
         }
