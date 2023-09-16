8396a641428b33a0189fd748857e85590627598f
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-17 01:21:22 +0900

    알고리즘 기초 100제-3번 정답

--- src/test/java/dev/kkkkkksssssaaaa/practice/algorithm/practice100/Q3.java
+++ src/test/java/dev/kkkkkksssssaaaa/practice/algorithm/practice100/Q3.java
@@ -16,6 +16,7 @@ class Q3 {
         int[] arr = {1, 2, 2, 3, 1, 4, 2, 2, 4, 3, 5, 3, 2};
 
         assertEquals(2, solution(arr));
+        assertEquals(2, answer(arr));
     }
 
     public int solution(int[] arr) {
@@ -36,5 +37,28 @@ class Q3 {
         list.sort(Map.Entry.comparingByValue());
 
         return list.get(list.size() - 1).getKey();
+    }
+
+    public int answer(int[] arr) {
+        int[] mode = new int[10];
+
+        for (int i = 0; i < mode.length; i++) {
+            mode[arr[i]]++;
+        }
+
+        int modeNum = 0;
+        int modeCount = 0;
+
+        for (int i = 0; i < mode.length; i++) {
+            if (modeCount < mode[i]) {
+                modeCount = mode[i];
+                modeNum = i;
+            }
+        }
+
+        System.out.println("최빈수 = " + modeNum);
+        System.out.println("출현횟수 = " + modeCount);
+
+        return modeNum;
     }
 }
