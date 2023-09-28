52de92a76a90d29fd4f70a13624b04550ebb70e0
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-28 18:28:41 +0900

    프로그래머스-타겟 넘버(정답 봄) - https://school.programmers.co.kr/questions/37431

---
+++ src/test/java/dev/kkkkkksssssaaaa/practice/algorithm/lv2/TargetNumber.java
@@ -0,0 +1,34 @@
+package dev.kkkkkksssssaaaa.practice.algorithm.lv2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// 타겟 넘버
+class TargetNumber {
+
+    @Test
+    void doTest() {
+        assertEquals(5, solution(new int[]{1, 1, 1, 1, 1}, 3));
+        assertEquals(2, solution(new int[]{4, 1, 2, 1}, 4));
+    }
+
+    static int result = 0;
+    public int solution(int[] numbers, int target) {
+        result = 0;
+
+        dfs(0, numbers, target, 0);
+        return result;
+    }
+
+    private static void dfs(int index, int[] num, int target, int sum) {
+        if(index == num.length) {
+            if(target == sum) {
+                result++;
+            }
+            return;
+        }
+        dfs(index + 1, num, target, sum + num[index]);
+        dfs(index + 1, num, target, sum - num[index]);
+    }
+}
