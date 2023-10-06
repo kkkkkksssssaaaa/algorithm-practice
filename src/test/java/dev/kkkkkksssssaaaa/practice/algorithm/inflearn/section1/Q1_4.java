e3d5a58287bdd74a29a63d384a9fe3c07e703dd0
kkkkkksssssaaaa <kofc312@naver.com>
2023-10-06 23:40:30 +0900

    인프런 알고리즘-1.4 단어 뒤집기

---
+++ src/test/java/dev/kkkkkksssssaaaa/practice/algorithm/inflearn/section1/Q1_4.java
@@ -0,0 +1,62 @@
+package dev.kkkkkksssssaaaa.practice.algorithm.inflearn.section1;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// 단어 뒤집기
+class Q1_4 {
+
+    @Test
+    void doTest() {
+        assertEquals("emiT", solution("Time"));
+    }
+
+    public String solution(String original) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : original.toCharArray()) {
+            stack.push(c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < original.length(); i++) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+        String[] result = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            String input = in.next();
+            Stack<Character> stack = new Stack<>();
+
+            for (char c : input.toCharArray()) {
+                stack.push(c);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < input.length(); j++) {
+                sb.append(stack.pop());
+            }
+
+            result[i] = sb.toString();
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+
+        return ;
+    }
+}
